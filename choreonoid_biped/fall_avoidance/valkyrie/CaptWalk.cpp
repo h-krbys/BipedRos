e1bd82e66a65208bc2e1de5aab48075b2e824bc2
dl-box <>
2019-12-20 15:13:05 +0900

    edit simplecontroller
    1s経過したらfootstep計算するように変更

--- choreonoid_biped/fall_avoidance/valkyrie/CaptWalk.cpp
+++ choreonoid_biped/fall_avoidance/valkyrie/CaptWalk.cpp
@@ -6,6 +6,8 @@
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 using namespace std;
 using namespace cnoid;
@@ -29,6 +31,7 @@ class CaptWalk : public SimpleController
 
   // ros setting
   ros::NodeHandle nh;
+  ros::Publisher  startPublisher, goalPublisher;
   ros::Subscriber footstepSubscriber;
 
   // rviz
@@ -40,6 +43,34 @@ class CaptWalk : public SimpleController
   Capt::Footstep footstep;
 
 public:
+  void startPublish(Vector3 pos){
+    geometry_msgs::PoseWithCovarianceStamped startPose;
+    startPose.header.frame_id         = "map";
+    startPose.header.stamp            = ros::Time::now();
+    startPose.pose.pose.position.x    = pos.x();
+    startPose.pose.pose.position.y    = pos.y();
+    startPose.pose.pose.position.z    = pos.z();
+    startPose.pose.pose.orientation.x = 0;
+    startPose.pose.pose.orientation.y = 0;
+    startPose.pose.pose.orientation.z = 0;
+    startPose.pose.pose.orientation.w = 1;
+    startPublisher.publish(startPose);
+  }
+
+  void goalPublish(Vector3 pos){
+    geometry_msgs::PoseStamped goalPose;
+    goalPose.header.frame_id    = "map";
+    goalPose.header.stamp       = ros::Time::now();
+    goalPose.pose.position.x    = pos.x();
+    goalPose.pose.position.y    = pos.y();
+    goalPose.pose.position.z    = pos.z();
+    goalPose.pose.orientation.x = 0;
+    goalPose.pose.orientation.y = 0;
+    goalPose.pose.orientation.z = 0;
+    goalPose.pose.orientation.w = 1;
+    goalPublisher.publish(goalPose);
+  }
+
   void footstepCallback( const nav_msgs::Path::ConstPtr &path){
     footstep.clear();
     footstepRef.clear();
@@ -63,6 +94,8 @@ public:
 
   virtual bool start() override
   {
+    startPublisher     = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1);
+    goalPublisher      = nh.advertise<geometry_msgs::PoseStamped>("/goal", 1);
     footstepSubscriber = nh.subscribe<nav_msgs::Path>("/footstep_planner/path", 10, &CaptWalk::footstepCallback, this);
     return true;
   }
@@ -86,6 +119,7 @@ public:
       io->enableInput(io->body()->link(i), LINK_POSITION);
     }
 
+    phase   = INIT;
     t       = 0.0;
     elapsed = 0.0;
 
@@ -99,6 +133,24 @@ public:
     publisher.setPose(ioBody);
     publisher.setFootstepRef(footstepRef);
     publisher.simulation(t);
+
+    switch( phase ) {
+    case INIT:
+      // posLLeg.translation().z() += 0.1 * dt;
+      // posRLeg.translation().z() += 0.1 * dt;
+      // if( com.z() <= 1.0 ) {
+      //   phase   = WAIT;
+      //   elapsed = 0.0;
+      // }
+      if(t > 1.0) {
+        startPublish(Vector3(0, 0, 0) );
+        goalPublish(Vector3(2, 0, 0) );
+        phase = WAIT;
+      }
+      break;
+    case WAIT:
+      break;
+    }
 
     t       += dt;
     elapsed += dt;
